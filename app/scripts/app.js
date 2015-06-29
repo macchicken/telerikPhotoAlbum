4cf1de1ae28443e3147ce1cd8145896636af671e
Barry Chen <log525lg100@googlemail.com>
2015-06-29 06:26:24 +0000

    Changes made by Barry Chen (log525lg100@googlemail.com). add Backend service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,51 @@
-(function() {
+(function () {
     document.addEventListener("deviceready", function () {
-        var app = new kendo.mobile.Application(document.body, { skin: "flat" });
+        var everlive = new Everlive("64SaMVJ3EQ8i0QhJ");
+        window.listview = kendo.observable({
+            // addImage: function () {
+            //     $("#images").data("kendoMobileListView").prepend(["images/08.jpg"]);
+            // }
+            addImage: function () {
+                var success = function (data) {
+                    // $("#images").data("kendoMobileListView").prepend(["data:image/jpeg;base64," + data]);
+                    everlive.Files.create({
+                        Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                        ContentType: "image/jpeg",
+                        base64: data
+                    }).then(loadPhotos);
+                };
+                var error = function () {
+                    navigator.notification.alert("Unfortunately we could not add the image");
+                };
+                var config = {
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    targetHeight: 400,
+                    targetWidth: 400
+                };
+                navigator.camera.getPicture(success, error, config);
+            }
+        });
+        var app = new kendo.mobile.Application(document.body, {
+            skin: "flat"
+        });
+        function loadPhotos() {
+            everlive.Files.get().then(function (data) {
+                var files = [];
+                data.result.forEach(function (image) {
+                    files.push(image.Uri);
+                });
+                $("#images").kendoMobileListView({
+                    dataSource: files,
+                    template: "<img src='#: data #'>"
+                });
+            });
+        }
+        loadPhotos();
+        // $("#images").kendoMobileListView({
+        //     dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
+        //     template: "<img src='#: data #'>"
+        // });
         navigator.splashscreen.hide();
     });
+
 }());
